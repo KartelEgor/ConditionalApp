8830f8fd22b668147aed5ca705fc12c6a9289314
Kartel Egor <dimurg34@gmail.com>
2023-09-28 10:33:32 +0300

    Integration Testing

--- src/test/java/ru/kartel/conditionalapp/ConditionalAppApplicationTests.java
+++ src/test/java/ru/kartel/conditionalapp/ConditionalAppApplicationTests.java
@@ -1,13 +1,43 @@
 package ru.kartel.conditionalapp;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ConditionalAppApplicationTests {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private static final GenericContainer<?> myDevApp = new GenericContainer<>("devapp:latest")
+            .withExposedPorts(8080);
+    private static final GenericContainer<?> myProdApp = new GenericContainer<>("prodapp:latest")
+            .withExposedPorts(8081);
+
+
+    @BeforeAll
+    public static void setUp() {
+        myDevApp.start();
+        myProdApp.start();
+    }
 
     @Test
-    void contextLoads() {
+    void testMyDev() {
+        String dev = "Current profile is dev";
+        ResponseEntity<String> entityFromDev = restTemplate.getForEntity("http://localhost:" + myDevApp.getMappedPort(8080), String.class);
+        Assertions.assertEquals(dev, entityFromDev.getBody());
+    }
+
+    @Test
+    void testMyProd() {
+        String prod = "Current profile is production";
+        ResponseEntity<String> entityFromDev = restTemplate.getForEntity("http://localhost:" + myProdApp.getMappedPort(8081), String.class);
+        Assertions.assertEquals(prod, entityFromDev.getBody());
     }
 
 }
